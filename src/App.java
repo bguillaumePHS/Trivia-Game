f0632cc2c4f5bf903ea4a57e562f939a7485baeb
Bennett Guillaume <bguillaume@powayusd.com>
2026-03-09 00:27:29 -0700

    First Commit

---
+++ src/App.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        String response = "";
+        int score = 0;
+
+
+        // Question 1
+        System.out.println("How many continents are there?");
+        System.out.println("  A) 5");
+        System.out.println("  B) 6");
+        System.out.println("  C) 7");
+        response = input.nextLine();
+        if(response.equalsIgnoreCase("C")) {
+            System.out.println("Correct! Nice work");
+            score += 1;
+        } else {
+            System.out.println("Sorry, not correct.");
+        }
+
+        // Question 2
+        System.out.println("What's the tallest building in the world?");
+        System.out.println("  A) The Empire State Building");
+        System.out.println("  B) Burj Khalifa");
+        System.out.println("  C) Sears Tower");
+        response = input.nextLine();
+        if(response.equalsIgnoreCase("B")) {
+            System.out.println("Correct! Nice work");
+            score += 1;
+        } else {
+            System.out.println("Sorry, not correct.");
+        }
+
+        // Question 3
+        System.out.println("What's the best Christmas movie");
+        System.out.println("  A) Die Hard");
+        System.out.println("  B) White Christmas");
+        System.out.println("  C) Frosty the Snowman");
+        response = input.nextLine();
+        if(response.equalsIgnoreCase("A")) {
+            System.out.println("Correct! Nice work");
+            score += 1;
+        } else {
+            System.out.println("Sorry, not correct.");
+        }
+
+        System.out.println();
+        System.out.println("You got " + score + "/3 correct!");
+
+        //Close the Scanner
+        input.close();
+    }
+}
